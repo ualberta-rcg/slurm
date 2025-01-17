07824b40a579a69d86724bd8b8d9c66f8ed59317
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-01-17 11:09:59 -0800

    Update entrypoint.sh

--- slurmdbd/entrypoint.sh
+++ slurmdbd/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Redirect logs to stdout and stderr for Kubernetes
+if [ -z "${LOG_FILE}" ] || [ "${LOG_FILE}" = "/var/log/slurm/slurmdbd.log" ]; then
+  export LOG_FILE=/dev/stdout
+fi
+
 # Replace variables in the template and write to /etc/slurm/slurmdbd.conf
 envsubst < /etc/slurm/slurmdbd.conf.template > /etc/slurm/slurmdbd.conf
 
