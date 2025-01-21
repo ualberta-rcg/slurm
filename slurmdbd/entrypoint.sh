470a211a3b84dc40f47a55b1b3c7d859b98e4787
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-01-21 11:49:03 -0800

    Update entrypoint.sh

--- slurmdbd/entrypoint.sh
+++ slurmdbd/entrypoint.sh
@@ -11,28 +11,12 @@ while IFS= read -r line; do
 done < /etc/slurm/slurmdbd.conf.template > /etc/slurm/slurmdbd.conf
 
 # Slurm 
-mkdir -p /var/log/slurm/
-touch /var/log/slurm/slurm-dbd.log
 chown slurm:slurm /etc/slurm/slurmdbd.conf
 chown slurm:slurm -R /var/log/slurm
 chmod 600 /etc/slurm/slurmdbd.conf
 
 # Munge
-mkdir -p /var/run/munge/
-mkdir -p /var/lib/munge/
-mkdir -p /var/log/munge/
-cp /etc/munge/munge/munge.key /etc/munge/
 chown munge:munge -R /etc/munge
-chown munge:munge -R /var/run/munge
-chown munge:munge -R /var/lib/munge
-chown munge:munge -R /var/log/munge
-chown munge:munge -R /run/munge
-chmod 700 /var/lib/munge
-chmod 700 /var/run/munge/
-chmod 750 -R /etc/munge
-chmod 400 /etc/munge/munge.key
-chmod 755 /run/munge
-cp /opt/software/munge/bin/* /usr/local/bin
 
 su -s /bin/bash -c "/opt/software/munge/sbin/munged --foreground --log-file=/var/log/munge/munge.log  &" munge
 
