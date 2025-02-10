bc6a7e1e00fa7e000da5294f5b5c3ab330cf1f35
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-02-09 21:53:13 -0800

    Update batch_template.sh

--- jupyterhub/batch_template.sh
+++ jupyterhub/batch_template.sh
@@ -12,14 +12,8 @@
 #SBATCH --constraint=""
 #SBATCH --chdir=/home/${username}
 
+set -euo pipefail
 echo "Starting JupyterHub on Slurm node"
 
-# Get the hostname of the allocated node
-export JUPYTERHUB_REMOTE_HOST=$(hostname)
-export JUPYTERHUB_REMOTE_PORT=${port}
-
-echo "JUPYTERHUB_REMOTE_HOST=${JUPYTERHUB_REMOTE_HOST}" >> /tmp/jupyterhub-${SLURM_JOB_ID}.log
-echo "JUPYTERHUB_REMOTE_PORT=${JUPYTERHUB_REMOTE_PORT}" >> /tmp/jupyterhub-${SLURM_JOB_ID}.log
-
-# Start Jupyter
-jupyterhub-singleuser --ip=0.0.0.0 --port=${port} --NotebookApp.token='' --NotebookApp.password=''
+# *** KEY CHANGE: Use batchspawner-singleuser correctly ***
+${BATCHSPAWNER_SINGLEUSER_CMD} "${JUPYTERHUB_API_URL}" "${JUPYTERHUB_BASE_URL}" "${JUPYTERHUB_COOKIE_NAME}" "${JUPYTERHUB_USER}" "${JUPYTERHUB_API_TOKEN}" "${JUPYTERHUB_SERVER_NAME}"
