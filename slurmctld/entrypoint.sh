595872865fb2cc5c6d0cde8844f252951d4cf830
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-02-16 09:21:16 -0800

    Update entrypoint.sh

--- slurmctld/entrypoint.sh
+++ slurmctld/entrypoint.sh
@@ -36,10 +36,6 @@ su -s /bin/bash -c "/usr/sbin/sssd -i -d 9 &" root
 # Wait briefly for munge to start
 sleep 2
 
-# Create hosts-original 
-cp /etc/hosts /etc/hosts-original
-cat /etc/hosts.d/hosts >> /etc/hosts
-
 # Verify that the slurmdbd is accessible before starting slurmctld
 timeout=60
 counter=0
@@ -52,62 +48,6 @@ while ! sacctmgr show cluster &>/dev/null; do
     fi
     echo "Waiting for slurmdbd to become available..."
 done
-
-# Start monitoring Slurm configuration files for changes
-CONFIG_FILES=(
-    "/etc/hosts.d/hosts"
-    "/etc/slurm/slurm.conf"
-    "/etc/slurm/gres.conf"
-    "/etc/slurm/cgroup.conf"
-)
-
-# Function to monitor configuration files
-monitor_config_files() {
-    declare -A FILE_CHECKSUMS
-
-    # Initialize checksums
-    for file in "${CONFIG_FILES[@]}"; do
-        if [[ -f $file ]]; then
-            FILE_CHECKSUMS["$file"]=$(md5sum "$file" | awk '{print $1}')
-        else
-            FILE_CHECKSUMS["$file"]=""
-        fi
-    done
-
-    echo "Monitoring configuration files for changes..."
-
-    while true; do
-        for file in "${CONFIG_FILES[@]}"; do
-            if [[ -f $file ]]; then
-                NEW_CHECKSUM=$(md5sum "$file" | awk '{print $1}')
-                if [[ "${FILE_CHECKSUMS["$file"]}" != "$NEW_CHECKSUM" ]]; then
-                    echo "Change detected in $file."
-
-                    # Special handling for /etc/hosts.d/warewulf
-                    if [[ "$file" == "/etc/hosts.d/warewulf" ]]; then
-                        echo "Updating /etc/hosts with contents from /etc/hosts.d/warewulf..."
-                        if [[ -f /etc/hosts-original ]]; then
-                            cat /etc/hosts-original > /etc/hosts  # Start with the original hosts file
-                            cat /etc/hosts.d/hosts >> /etc/hosts  # Append warewulf contents
-                            echo "/etc/hosts updated successfully."
-                        else
-                            echo "Error: /etc/hosts-original does not exist!"
-                        fi
-                    else
-                        echo "Reloading SLURM configuration due to change in $file..."
-                        su -s /bin/bash slurm -c "/usr/bin/scontrol reconfigure"
-                    fi
-
-                    FILE_CHECKSUMS["$file"]="$NEW_CHECKSUM"
-                fi
-            fi
-        done
-        sleep 5  # Adjust the interval as needed
-    done
-}
-
-# Start monitoring in the background
-(monitor_config_files) &
 
 # Run slurmctld as the slurm user
 exec su -s /bin/bash slurm -c "/usr/sbin/slurmctld $*"
