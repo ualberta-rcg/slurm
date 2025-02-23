076be8f8ac81df0aa2a14ae6c483f0fdeb59754d
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-02-22 16:38:08 -0800

    Create edit_coredns_commands.sh

---
+++ scripts/edit_coredns_commands.sh
@@ -0,0 +1,8 @@
+# Use 
+
+# The following Command Edits the config map for CoreDNS
+#kubectl -n kube-system edit configmap rke2-coredns-rke2-coredns
+# The Following Command Applies the ConfigMap Edits
+#kubectl -n kube-system rollout restart deployment rke2-coredns-rke2-coredns
+
+
