e9f9a481348ff76cdbea05a7cf755860b473f688
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-02-22 16:39:46 -0800

    Update edit_coredns_commands.sh

--- scripts/edit_coredns_commands.sh
+++ scripts/edit_coredns_commands.sh
@@ -1,8 +1,5 @@
-# Use 
-
+# Use example-rke2-coredns.yaml as an example on how to add Hosts to Core DNS
 # The following Command Edits the config map for CoreDNS
 #kubectl -n kube-system edit configmap rke2-coredns-rke2-coredns
 # The Following Command Applies the ConfigMap Edits
 #kubectl -n kube-system rollout restart deployment rke2-coredns-rke2-coredns
-
-
