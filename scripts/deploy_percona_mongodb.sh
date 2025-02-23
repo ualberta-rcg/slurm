31654fcc029b65e7988772744a6cc1d183cb0d18
Rahim Khoja <13703075+rahimkhoja@users.noreply.github.com>
2025-02-22 23:32:53 -0800

    Update deploy_percona_mongodb.sh

--- scripts/deploy_percona_mongodb.sh
+++ scripts/deploy_percona_mongodb.sh
@@ -1,3 +1,3 @@
 kubectl create namespace percona-mongo-cluster
 helm install mongodb-operator percona/psmdb-operator --namespace percona-mongo-cluster
-helm install mongodb-graylog percona/psmdb-db --namespace percona-mongo-cluster -f mongodb-graylog-values.yaml
+helm install mongodb-graylog percona/psmdb-db --namespace percona-mongo-cluster -f percona-mongodb-graylog-values.yaml
